d05216e148b5e0fc79b0fc1e9d2495b6216d5bbd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-09-29 10:08:44 +0200

    rhodei: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: moto g62 5G Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_rhodei.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_rhodei-user \
+    omni_rhodei-userdebug \
+    omni_rhodei-eng
