4b77d76da4759bbe1fb4e3c4bcc35c49d97b90c2
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-25 17:50:58 +0600

    Being up

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_rhodei-user
-add_lunch_combo omni_rhodei-userdebug
-add_lunch_combo omni_rhodei-eng
+add_lunch_combo twrp_rhodei-user
+add_lunch_combo twrp_rhodei-userdebug
+add_lunch_combo twpr_rhodei-eng
